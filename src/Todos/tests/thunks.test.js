0c0742c2e4ae123c15fc598352bba5c86f3de574
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-08-25 14:58:38 +0100

    finished todo list app

--- src/Todos/tests/thunks.test.js
+++ src/Todos/tests/thunks.test.js
@@ -1,6 +1,29 @@
+import 'node-fetch';
+import fetchMock from 'fetch-mock';
 import {expect} from 'chai'; 
 import {fetchTodos} from '../thunk';
+import {
+    fetchTodoInProgress, 
+    fetchTodoSuccess
+} from '../actions';
+import sinon from 'sinon';
 
-describe('The fetchTodos thumk', ()=>{
+describe('The fetchTodos thunk', ()=>{
+    it('Dispatches the correct actions in the success scenario',async ()=>{
+        const fakeDispatch = sinon.spy();
 
+        const fakeTodos = [{text:'1'},{text:'2'}];
+        fetchMock.get('http://localhost:8080/todos',fakeTodos);
+
+        const expectedFirstAction = fetchTodoInProgress();  
+        const expectedSecondAction = fetchTodoSuccess();  
+        expectedSecondAction.payload.todos=fakeTodos;
+
+        await fetchTodos()(fakeDispatch);
+        
+        expect(fakeDispatch.getCall(0).args[0]).to.deep.equal(expectedFirstAction);
+        expect(fakeDispatch.getCall(1).args[0]).to.deep.equal(expectedSecondAction);
+
+        fetchMock.reset();
+    })
 });
